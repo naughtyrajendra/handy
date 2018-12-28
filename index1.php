9b05ee359e2f85f8f600388a63d8228db1d4171d
naughtyrajendra <46136601+naughtyrajendra@users.noreply.github.com>
2018-12-28 13:05:09 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="form.php" method="post">
+	<div>
+		fname<input type="text" name="fname">
+	</div><br>
+	<div>
+		lname<input type="text" name="lname">
+	</div><br>
+	<input type="button" name="" value="sumit">
+
+	
+</form>
+
+</body>
+</html>
